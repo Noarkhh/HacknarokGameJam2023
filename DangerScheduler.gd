d082719add62d63070810885227e9a76145d9fe5
Noarkhh <jakubpryc@student.agh.edu.pl>
2023-04-01 18:47:29 +0200

    minor changes

--- DangerScheduler.gd
+++ DangerScheduler.gd
@@ -1,6 +1,6 @@
 extends Timer
 
-var segments_scenes = [preload("res://segment_barrel_stack.tscn")]
+var segments_scenes = [preload("res://segment_barrel_stack.tscn"), preload("res://segment_market.tscn")]
 
 var segment_queue = []
 
@@ -9,7 +9,7 @@ var segment_speed = 500.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_autostart(true)
-	wait_time = 1275 / segment_speed
+	wait_time = 1270.0 / segment_speed
 	timeout.connect(on_timer_timeout)
 	
 
